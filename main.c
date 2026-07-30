131f9161aaeaecc3a9eb84a759c94c76c505da15
kareem-hagag <engkareemhagag@gmail.com>
2026-07-30 17:15:20 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,27 @@
+/*************************************************************/
+/************ Auther : kareem hagag ******************/
+/************ date : 29/7/2026 ******************/
+/************ Title: MAIN ********************/
+/**************************************************************/
+#include "../std_lib/Std_Types.h"
+#include "../std_lib/Bit_Math.h"
+#include "../mcal/Dio_Int.h"
+#include "../mcal/Dio_Private.h"
+#include "../mcal/Dio_Config.h"
+#include <avr/delay.h>
+
+int main(void)
+{
+	DIO_voidInitialization();
+	DIO_voidSetPinDirection(DIO_PORTA, PIN0, OUTPUT);
+
+	while(1)
+	{
+		DIO_voidSetpinValue(DIO_PORTA, PIN0, HIGH);
+		_delay_ms(100);
+		DIO_voidSetpinValue(DIO_PORTA, PIN0, LOW);
+		_delay_ms(100);
+	}
+
+	return 0;
+}
